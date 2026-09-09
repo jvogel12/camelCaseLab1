826def23b339551264455fab6ff3a46c526eeabc
jvogel12 <vj8711cc@go.minnstate.edu>
2026-09-09 17:47:48 -0500

    add banner welcome message at start

--- camel.py
+++ camel.py
@@ -1,14 +1,20 @@
 def camelcase(sentence):
-    title_case = sentence,title()
-    upper_camel_cased = title_case.replace (' ','')
+    title_case = sentence.title()
+    upper_camel_cased = title_case.replace(' ', '')
 
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    '''display program name'''
+    message = "Awesome Camel Case Converter"
+    stars = '*' * len(message)
+    print(f'\n{stars}\n{message}\n{stars}\n')
+
 def main():
+    banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
 
 if __name__ == '__main__':
     main()
-    
